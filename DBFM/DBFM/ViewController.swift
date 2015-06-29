226da10c7f183c29c49ecb4e771071b4d4a9e896
张青明 <laznrbfe@zhangqingmingdeMacBook-Pro.local>
2015-06-29 09:20:10 +0800

    初始化工程

---
+++ DBFM/DBFM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBFM
+//
+//  Created by 张青明 on 15/6/28.
+//  Copyright (c) 2015年 张青明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
